9a532950b791a0aca37fbbef7a82162caeb307e1
Liann Maicoo <liannmaicoo@gmail.com>
2025-11-25 05:23:43 +0000

    Updated Category & Question class attributes

--- game_system/src/main/java/jeopardy_game/Category.java
+++ game_system/src/main/java/jeopardy_game/Category.java
@@ -6,24 +6,15 @@ public class Category {
     private String name;
     private List<Question> questions;
 
-    public Category(String name, List<Question> questions) {
+    public Category(String name) {
         this.name = name;
-        this.questions = questions;
     }
-
 
     // Getters
-    public String getName() {
-        return name;
-    }
-    public List<Question> getQuestions() {
-        return questions;
-    }
+    public String getName() {return name;}
+    public List<Question> getQuestions() {return questions;}
 
     // Setters
-    public void setName(String name){
-        this.name = name;
-    }
     public void addQuestion(Question newQuestion){
         this.questions.add(newQuestion); 
     }
--- game_system/src/main/java/jeopardy_game/Question.java
+++ game_system/src/main/java/jeopardy_game/Question.java
@@ -1,54 +1,31 @@
 package jeopardy_game;
 
+import java.util.Map;
+
 public class Question {
     private String questionStr;
-    private String options[];
-    private char answer;
-    private int points;
+    private int value;
+    private Map<String, String> options;
+    private String answer;
     private boolean answered;
 
-    public Question(String questionStr, String[] options, char answer, int points) {
+    public Question(String questionStr, int value, Map<String, String> options, String answer) {
         this.questionStr = questionStr;
+        this.value = value;
         this.options = options;
         this.answer = answer;
-        this.points = points;
         this.answered = false;
     }
 
     // Getters
-    public boolean isAnswered() {
-        return answered;
-    }
-    public String getQuestionStr() {
-        return questionStr;
-    }
-    public String[] getOptions() {
-        return options;
-    }
-    public char getAnswer() {
-        return answer;
-    }
-    public int getPoints() {
-        return points;
-    }
-
+    public boolean isAnswered() {return answered;}
+    public String getQuestionStr() {return questionStr;}
+    public Map<String, String> getOptions() {return options;}
+    public String getAnswer() {return answer;}
+    public int getValue() {return value;}
 
     // Setters
-    public void setQuestionStr(String questionStr) {
-        this.questionStr = questionStr;
-    }
-    public void setOptions(String[] options) {
-        this.options = options;
-    }
-    public void setAnswer(char answer) {
-        this.answer = answer;
-    }
     public void setAnswered(boolean answered) {
         this.answered = answered;
-    }
-    public void setPoints(int points) {
-        this.points = points;
-    }
-
-    
+    }  
 }
